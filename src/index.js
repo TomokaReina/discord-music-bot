5e3938894eb0c02d0c0e7283f7a41c2a60853644
Renno Sullivan <renno.hwang@gmail.com>
2020-06-26 10:25:45 -0500

    format change and downloaded deps
    finally figured out how to somewhat use .env files. bless

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const { readdirSync } = require('fs');
 const { join } = require('path');
 const MusicClient = require('./struct/Client');
 const { Collection } = require('discord.js');
-const client = new MusicClient({ token: process.env.DISCORD_TOKEN, prefix: process.env.DISCORD_PREFIX });
+const client = new MusicClient({
+	token: process.env.DISCORD_TOKEN,
+	prefix: process.env.DISCORD_PREFIX
+});
 
 const commandFiles = readdirSync(join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
